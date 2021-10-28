ae21ce4655f4e00626bd033ad26bfd45d08a5d8f
imalgrab <igormalgrab@gmail.com>
2021-10-28 18:06:02 +0200

    Update package.json, add current picker progress

---
+++ src/components/ColorPicker.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
+import LinearGradient, {
+  LinearGradientProps,
+} from 'react-native-linear-gradient';
+import Animated, {
+  interpolateColor,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+
+const {width} = Dimensions.get('window');
+
+const COLOR_PICKER_WIDTH = 0.9 * width;
+const CIRCLE_PICKER_SIZE = 45;
+const INNER_CIRCLE_SIZE = 35;
+
+const MAX_WIDTH = COLOR_PICKER_WIDTH - CIRCLE_PICKER_SIZE;
+
+type PositionContext = {
+  translateX: number;
+  translateY: number;
+};
+
+interface ColorPickerProps extends LinearGradientProps {
+  colors: string[];
+}
+
+const ColorPicker: React.FC<ColorPickerProps> = ({colors, start, end}) => {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const scale = useSharedValue(1);
+
+  const adjustedTranslateX = useDerivedValue(() => {
+    return Math.min(Math.max(translateX.value, 0), MAX_WIDTH);
+  });
+
+  const pickerAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {translateX: adjustedTranslateX.value},
+        {translateY: translateY.value},
+        {scale: scale.value},
+      ],
+    };
+  });
+
+  const innerPickerAnimatedStyle = useAnimatedStyle(() => {
+    const inputRange = colors.map(
+      (_, index) => (index / colors.length) * COLOR_PICKER_WIDTH,
+    );
+    const backgroundColor = interpolateColor(
+      translateX.value,
+      inputRange,
+      colors,
+    );
+    return {
+      backgroundColor,
+    };
+  });
+
+  const onDrag = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    PositionContext
+  >({
+    onStart: (_, context) => {
+      context.translateX = adjustedTranslateX.value;
+      translateY.value = withSpring(-CIRCLE_PICKER_SIZE);
+      scale.value = withSpring(1.5);
+    },
+    onActive: (event, context) => {
+      translateX.value = event.translationX + context.translateX;
+    },
+    onEnd: () => {
+      translateY.value = withSpring(0);
+      scale.value = withSpring(1);
+    },
+  });
+
+  return (
+    <PanGestureHandler onGestureEvent={onDrag}>
+      <Animated.View style={styles.container}>
+        <LinearGradient
+          style={styles.gradient}
+          colors={colors}
+          start={start}
+          end={end}
+        />
+        <Animated.View style={[styles.picker, pickerAnimatedStyle]}>
+          <Animated.View
+            style={[styles.innerPicker, innerPickerAnimatedStyle]}
+          />
+        </Animated.View>
+      </Animated.View>
+    </PanGestureHandler>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  gradient: {
+    width: COLOR_PICKER_WIDTH,
+    height: 40,
+    borderRadius: 20,
+  },
+  picker: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    width: CIRCLE_PICKER_SIZE,
+    height: CIRCLE_PICKER_SIZE,
+    borderRadius: CIRCLE_PICKER_SIZE / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  innerPicker: {
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,.2)',
+    width: INNER_CIRCLE_SIZE,
+    height: INNER_CIRCLE_SIZE,
+    borderRadius: INNER_CIRCLE_SIZE / 2,
+  },
+});
+export default ColorPicker;
--- src/screens/ColorPickerAnimation.tsx
+++ src/screens/ColorPickerAnimation.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
+import ColorPicker from '../components/ColorPicker';
+
+const COLORS = [
+  'red',
+  'purple',
+  'blue',
+  'cyan',
+  'green',
+  'yellow',
+  'orange',
+  'white',
+  'black',
+];
+
+const BACKGROUND_COLOR = 'rgba(1,1,1,.9)';
 
 interface ColorPickerAnimationProps {}
 
 const ColorPickerAnimation: React.FC<ColorPickerAnimationProps> = () => {
   return (
     <View style={styles.container}>
-      <Text>Color picker animation</Text>
+      <View style={styles.top}></View>
+
+      <View style={styles.bottom}>
+        <ColorPicker colors={COLORS} start={{x: 0, y: 0}} end={{x: 1, y: 0}} />
+      </View>
     </View>
   );
 };
@@ -14,6 +33,17 @@ const ColorPickerAnimation: React.FC<ColorPickerAnimationProps> = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: BACKGROUND_COLOR,
+  },
+  top: {
+    flex: 3,
+    backgroundColor: '#fff',
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: BACKGROUND_COLOR,
   },
 });
 export default ColorPickerAnimation;
